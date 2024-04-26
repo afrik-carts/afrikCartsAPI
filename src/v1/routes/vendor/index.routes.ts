114c88754d2e268e6621a612190b431d7b9a4a6a
Anurigwo Chibuzor <cibuzoraustine50@gmail.com>
2024-04-26 15:19:24 +0100

    Update index.routes.ts

--- src/v1/routes/vendor/index.routes.ts
+++ src/v1/routes/vendor/index.routes.ts
@@ -2,7 +2,7 @@ import express from  'express'
 import authRoutes from "./auth.routes.js" 
 import accountRoutes from "./account.routes.js" 
 import orderRoutes from "./order.routes.js" 
-import mwnuRoutes from "./menu.routes.js" 
+import menuRoutes from "./menu.routes.js" 
 import only_vendor from '../../middlewares/only_vendor.middleware.js';
 
 const router = express.Router();
@@ -10,6 +10,6 @@ const router = express.Router();
 router.use('/auth', authRoutes);
 router.use('/account', only_vendor, accountRoutes);
 router.use('/orders', only_vendor, orderRoutes);
-router.use('/menu', only_vendor, mwnuRoutes);
+router.use('/menu', only_vendor, menuRoutes);
 
 export default router;
