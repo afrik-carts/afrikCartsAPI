24c521d7cca51c3181b56da2ece01bed63e6e26d
Anurigwo Chibuzor <cibuzoraustine50@gmail.com>
2024-04-26 13:33:18 +0100

    Update index.ts

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,12 +1,12 @@
 import express from  'express'
 import adminRoutes from "./admin/index.routes.js" 
 import customerRoutes from "./customer/index.routes.js" 
-import logisticRoutes from "./vendor/index.routes.js" 
+import vendorRoutes from "./vendor/index.routes.js" 
 
 const router = express.Router();
 
 router.use('/customer', customerRoutes);
-router.use('/logistic', logisticRoutes);
+router.use('/venor', vendorRoutes);
 router.use('/admin', adminRoutes);
 
 export default router;
